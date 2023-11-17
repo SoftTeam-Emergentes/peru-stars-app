fa40e75154cedbc0f6464869a9e3bb3cd175f1b5
Brayan Cruces <brayan2259@gmail.com>
2023-11-16 20:42:14 -0500

    WIP: Artist Profile

---
+++ src/artist/services/artist-api.service.js
@@ -0,0 +1,24 @@
+import http from '../../shared/service/http-common';
+
+export class ArtistsApiService {
+    getAll() {
+        return http.get('/artists');
+    }
+
+    getById(id) {
+        return http.get(`/artists/${id}`);
+    }
+
+    create(data) {
+        return http.post('/artists', data);
+    }
+
+    update(id, data) {
+        return http.put(`/artists/${id}`, data);
+    }
+
+    delete(id) {
+        return http.delete(`/artists/${id}`);
+    }
+
+}
